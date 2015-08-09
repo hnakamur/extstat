3826b1604b234af819ca5ae8c811de5b3065c75e
Hiroaki Nakamura <hnakamur@gmail.com>
2015-08-09 11:19:00 +0900

    Use os.Chtimes to modify access and modification times.
    However the creation time is also modified on OS X Yosemite with
    go version 1.5rc1.
    
    --- FAIL: TestNew (6.01s)
            extstat_test.go:40: wait for changing mtime...
            extstat_test.go:53: wait for changing atime...
            extstat_test.go:56: test
            extstat_test.go:75: ctime0: 2015-08-09 11:23:17 +0900 JST
            extstat_test.go:76: ctime is wrong: 2015-08-09
    11:23:16.125946461 +0900 JST < 2015-08-09 11:23:23 +0900 JST <
    2015-08-09 11:23:18.125946461 +0900 JST

--- extstat_test.go
+++ extstat_test.go
@@ -3,13 +3,14 @@ package extstat
 import (
 	"io/ioutil"
 	"os"
+	"runtime"
 	"testing"
 	"time"
-	"runtime"
 )
 
 func TestNew(t *testing.T) {
-	f, err := ioutil.TempFile("", "")
+	//f, err := ioutil.TempFile("", "")
+	f, err := os.Create("hoge")
 	if err != nil {
 		t.Error(err)
 		return
@@ -17,15 +18,24 @@ func TestNew(t *testing.T) {
 	filePath := f.Name()
 	f.Write([]byte("test"))
 	defer os.Remove(filePath)
+
 	now := time.Now()
 
 	ctimeBefore := now.Add(+time.Second)
-	ctimeAfter :=  now.Add(-time.Second)
-	mtimeBefore := now.Add(time.Second*4)
-	mtimeAfter := now.Add(time.Second*2)
-	atimeBefore := now.Add(time.Second*7)
-	atimeAfter := now.Add(time.Second*5)
+	ctimeAfter := now.Add(-time.Second)
+	mtimeBefore := now.Add(time.Second * 4)
+	mtimeAfter := now.Add(time.Second * 2)
+	atimeBefore := now.Add(time.Second * 7)
+	atimeAfter := now.Add(time.Second * 5)
 	f.Close()
+
+	stat0, err := os.Stat(filePath)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	extStat0 := New(stat0)
 
 	t.Log("wait for changing mtime...")
 	time.Sleep(time.Second * 3)
@@ -37,6 +47,8 @@ func TestNew(t *testing.T) {
 	}
 	fileForModify.Write([]byte("hello world"))
 	fileForModify.Close()
+	modifiedTime := time.Now().Local()
+	err = os.Chtimes(filePath, modifiedTime, modifiedTime)
 
 	t.Log("wait for changing atime...")
 	time.Sleep(time.Second * 3)
@@ -46,6 +58,8 @@ func TestNew(t *testing.T) {
 		t.Error(err)
 		return
 	}
+	accessedTime := time.Now().Local()
+	err = os.Chtimes(filePath, accessedTime, modifiedTime)
 
 	stat, err := os.Stat(filePath)
 	if err != nil {
@@ -58,13 +72,16 @@ func TestNew(t *testing.T) {
 	// plan9 doesn't have correct ctime attribute
 	if runtime.GOOS != "plan9" {
 		if !extStat.CreatedTime.Before(ctimeBefore) || !extStat.CreatedTime.After(ctimeAfter) {
+			t.Error("ctime0:", extStat0.CreatedTime)
 			t.Error("ctime is wrong:", ctimeAfter, "<", extStat.CreatedTime, "<", ctimeBefore)
 		}
 	}
 	if !extStat.ModTime.Before(mtimeBefore) || !extStat.ModTime.After(mtimeAfter) {
+		t.Error("mtime0:", extStat0.ModTime)
 		t.Error("mtime is wrong:", mtimeAfter, "<", extStat.CreatedTime, "<", mtimeBefore)
 	}
 	if !extStat.AccessTime.Before(atimeBefore) || !extStat.AccessTime.After(atimeAfter) {
+		t.Error("atime0:", extStat0.AccessTime)
 		t.Error("atime is wrong:", atimeAfter, "<", extStat.CreatedTime, "<", atimeBefore)
 	}
 }
